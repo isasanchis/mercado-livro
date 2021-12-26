98cc0277e44c9a5fb4efe001272b2c8c795ccf98
Isabella <isasanchis3@gmail.com>
2021-12-26 19:09:34 -0300

    adicionado nova mensagem na tela de erro 404

--- src/views/NotFound/index.jsx
+++ src/views/NotFound/index.jsx
@@ -6,6 +6,7 @@ export const NotFound = () => {
         <C.NotFound>
             <h1>404</h1>
             <h2>Ops! Página não encontrada.</h2>
+            <p>Tente novamente mais tarde.</p>
         </C.NotFound>
     );
 };
